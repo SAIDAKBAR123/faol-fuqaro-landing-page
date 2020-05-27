bcd6e4821b096500fb2a24aebe699345508a0f5d
saidakbar123 <smahmudxojaev@gmail.com>
2020-05-27 17:17:28 +0500

    api set up

--- js/MainVue.js
+++ js/MainVue.js
@@ -1,5 +1,5 @@
 const i18n = new VueI18n({
-    locale: localStorage.getItem('lang') || 'ru',
+    locale: localStorage.getItem('lang') || 'uz',
     falbacklocale: 'uz',
     messages: {
       'uz': {
@@ -90,39 +90,15 @@ const app = new Vue({
     el: '#app',
     i18n,
     data: {
+        lang: localStorage.getItem('lang') || 'uz',
         report: {
-            all: '',
-            closed: '',
-            rejected: '',
-            reviewed: ''
+            all: 0,
+            closed: 0,
+            rejected: 0,
+            reviewed: 0
         },
         message: 'Hello Vue!',
-        news: [
-            {
-                title: '«Ўрикзор» бозори карантинга ёпилди. Сотувчилардан бирида вирус аниқланган',
-                body: 'Тошкентдаги «Ўрикзор» савдо мажмуасида коронавирус инфекцияси аниқланди, бозор вақтинчалик ёпилди. Соғлиқни сақлаш вазирлиги ҳушёрликка чақирмоқда.',
-                image: 'https://storage.kun.uz/source/6/3PA1oJO0ndYxXA4f867Qu_hIIossEiQV.jpg',
-                path: ''
-            },
-            {
-                title: 'Ўзбекистон ва Туркия президентлари икки томонлама муносабатларни муҳокама қилди',
-                body: 'Бугун, 23 май куни  Ўзбекистон президенти Шавкат Мирзиёев Туркия президенти Ражаб Тоййиб Эрдўған билан телефон орқали мулоқот қилди.  Бу ҳақда Ўзбекистон президенти матбуот хизмати хабар берди.',
-                image: 'https://storage.kun.uz/source/6/NVIF5Z3vIAAojfGQjsGvOJTOahab8qta.jpg',
-                path: ''
-            },
-            {
-                title: 'Тожикистонга ўтган тўрт ойда берилган гуманитар ёрдам миқдори маълум қилинди. Энг кўп ёрдам Ўзбекистондан',
-                body: 'Тожикистонга январь-апрель ойларида 4 минг тонна ёрдам юки, жумладан, тиббиёт воситалари, дори-дармонлар ва озиқ-овқат маҳсулотлари етказиб берилган.',
-                image: 'https://storage.kun.uz/source/6/NN2PyIyv9hnMvT3TW6fbQYIMTkVHIQp6.jpg',
-                path: ''
-            },
-            {
-                title: 'Тожикистонга ўтган тўрт ойда берилган гуманитар ёрдам миқдори маълум қилинди. Энг кўп ёрдам Ўзбекистондан',
-                body: 'Тожикистонга январь-апрель ойларида 4 минг тонна ёрдам юки, жумладан, тиббиёт воситалари, дори-дармонлар ва озиқ-овқат маҳсулотлари етказиб берилган.',
-                image: 'https://storage.kun.uz/source/6/NN2PyIyv9hnMvT3TW6fbQYIMTkVHIQp6.jpg',
-                path: ''
-            }
-        ]
+        news: []
     },
     methods: {
         setLocale( lang ) {
@@ -139,10 +115,22 @@ const app = new Vue({
         }
     },
     created() {
+
         fetch('https://server.sunbet.uz/api/counts')
         .then(response => response.json())
          .then(data => {
              this.report = data
-         });
+         }).catch( err => {
+            console.log(err)
+        })
+
+         fetch(`http://api.sunbet.uz/api/posts/public?lang=${this.lang }`)
+        .then(response => response.json())
+         .then(data => {
+             console.log(data)
+             this.news = data
+         }).catch( err => {
+             console.log(err)
+         })
     }
 })
